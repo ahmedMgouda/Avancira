89b2938946505161d3abfbab4fc88f468223c0fb
Ahmed Mostafa <eng.gouda91@gmail.com>
2025-07-04 14:02:19 +0300

    fix static file routing for production

--- Frontend.Angular/src/app/environments/environment.prod.ts
+++ Frontend.Angular/src/app/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    baseApiUrl: 'https://www.avancira.com',
-    apiUrl: `https://www.avancira.com/api`,
-    frontendUrl: 'https://www.avancira.com',
-    useSignalR: true,
-  };
-  
+  baseApiUrl: 'https://www.avancira.com',
+  apiUrl: `https://www.avancira.com/api`,
+  frontendUrl: 'https://www.avancira.com',
+  useSignalR: true,
+};
