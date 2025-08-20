a633abcbc09ce23f65febded58cad1183118295a
Ahmed Mostafa <eng.gouda91@gmail.com>
2025-08-20 18:56:28 +0300

    Increase reset password minimum length

--- api/Avancira.Application/Identity/Users/Validators/ResetPasswordValidator.cs
+++ api/Avancira.Application/Identity/Users/Validators/ResetPasswordValidator.cs
@@ -12,7 +12,8 @@ public class ResetPasswordValidator : AbstractValidator<ResetPasswordDto>
 
         RuleFor(x => x.Password)
             .NotEmpty()
-            .MinimumLength(6)
+            .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.")
             .Matches("[A-Z]")
                 .WithMessage("Password must contain at least one uppercase letter.")
             .Matches("[a-z]")
