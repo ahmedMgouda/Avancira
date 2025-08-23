5c72134be13e30b715f4aff641f9bbf50e769ce6
Ahmed Mostafa <eng.gouda91@gmail.com>
2025-08-23 14:36:44 +0300

    fix: correct styleUrls metadata

--- Frontend.Angular/src/app/components/social-login-buttons/social-login-buttons.component.ts
+++ Frontend.Angular/src/app/components/social-login-buttons/social-login-buttons.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 import { SocialProvider } from '../../models/social-provider';
 
 @Component({
   selector: 'app-social-login-buttons',
   imports: [CommonModule],
   templateUrl: './social-login-buttons.component.html',
-  styleUrl: './social-login-buttons.component.scss'
+  styleUrls: ['./social-login-buttons.component.scss']
 })
 export class SocialLoginButtonsComponent {
   @Output() google = new EventEmitter<void>();
