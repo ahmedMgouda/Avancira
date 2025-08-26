2716c77e80d4c88affa4eca5776516a819126b44
Ahmed Mostafa <eng.gouda91@gmail.com>
2025-08-26 23:46:59 +0300

    Set refresh token cookie secure flag based on environment

--- api/Avancira.API.Tests/BaseApiControllerTests.cs
+++ api/Avancira.API.Tests/BaseApiControllerTests.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
 
 public class BaseApiControllerTests
 {
@@ -19,7 +22,15 @@ public class BaseApiControllerTests
     public void SetRefreshTokenCookie_WithExpiry_SetsCookieWithOptions()
     {
         var controller = new TestController();
-        var httpContext = new DefaultHttpContext();
+        var envMock = new Mock<IHostEnvironment>();
+        envMock.SetupGet(e => e.EnvironmentName).Returns(Environments.Production);
+
+        var services = new ServiceCollection();
+        services.AddSingleton(envMock.Object);
+        var httpContext = new DefaultHttpContext
+        {
+            RequestServices = services.BuildServiceProvider()
+        };
         controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
 
         var expires = DateTime.UtcNow.AddDays(1);
@@ -39,7 +50,15 @@ public class BaseApiControllerTests
     public void SetRefreshTokenCookie_WithoutExpiry_SetsSessionCookie()
     {
         var controller = new TestController();
-        var httpContext = new DefaultHttpContext();
+        var envMock = new Mock<IHostEnvironment>();
+        envMock.SetupGet(e => e.EnvironmentName).Returns(Environments.Production);
+
+        var services = new ServiceCollection();
+        services.AddSingleton(envMock.Object);
+        var httpContext = new DefaultHttpContext
+        {
+            RequestServices = services.BuildServiceProvider()
+        };
         controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
 
         controller.InvokeSetRefreshTokenCookie("token456", null);
@@ -51,5 +70,27 @@ public class BaseApiControllerTests
         setCookie.Should().Contain("samesite=none");
         setCookie.Should().Contain("secure");
         setCookie.Should().NotContain("expires=");
+    }
+
+    [Fact]
+    public void SetRefreshTokenCookie_InDevelopment_DoesNotSetSecure()
+    {
+        var controller = new TestController();
+        var envMock = new Mock<IHostEnvironment>();
+        envMock.SetupGet(e => e.EnvironmentName).Returns(Environments.Development);
+
+        var services = new ServiceCollection();
+        services.AddSingleton(envMock.Object);
+        var httpContext = new DefaultHttpContext
+        {
+            RequestServices = services.BuildServiceProvider()
+        };
+        controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
+
+        controller.InvokeSetRefreshTokenCookie("token789", null);
+
+        var setCookie = httpContext.Response.Headers["Set-Cookie"].ToString().ToLowerInvariant();
+        setCookie.Should().Contain("refreshtoken=token789");
+        setCookie.Should().NotContain("secure");
     }
 }
