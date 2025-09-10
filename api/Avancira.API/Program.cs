5f0a6a85fa59ba10a4de01b3febca0b1db1c4a2c
Ahmed Mostafa <eng.gouda91@gmail.com>
2025-09-10 03:05:31 +0300

    Remove redundant default endpoint mapping

--- api/Avancira.API/Program.cs
+++ api/Avancira.API/Program.cs
@@ -55,8 +55,6 @@ public partial class Program {
 
         var app = builder.Build();
 
-        app.MapDefaultEndpoints();
-
         app.UseAvanciraFramework();
 
         app.UseHttpsRedirection();
