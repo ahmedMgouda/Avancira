f0c4a6d35e8efb6798f9c366fac87154a0a2c144
ahmedMgouda <eng.gouda91@gmail.com>
2025-10-23 18:00:45 +0300

    remove entities

--- api/Avancira.Domain/Listings/ListingCategory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Avancira.Domain.Catalog;
-public class ListingCategory
-{
-    public Guid ListingId { get; set; }
-    public Guid CategoryId { get; set; }
-    public virtual Listing Listing { get; set; } = default!;
-    public virtual Category Category { get; set; } = default!;
-}
